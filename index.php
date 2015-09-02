70e5ee0788578a465b2443fa1867970389ba92f0
wordpressmonk <kevin@wordpressmonk.com>
2015-09-02 17:22:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ echo "<br/>";
 echo  "<p align='center' style='color:green'>".$_SERVER['SERVER_NAME']."</p>";
 $s = $_REQUEST['s'];
 $u = $_REQUEST['u'];
-$url = "http://retargetbomb.herokuapp.com/link/?s=$s&u=$u";
+if($s!='' && $u!=''){
+$url = "http://retargetbomb.herokuapp.com/link/?s=$s&u=$u";	
+}else {
+$url = "http://retargetbomb.herokuapp.com/link/";		
+}
 file_get_content($url);
 ?>
