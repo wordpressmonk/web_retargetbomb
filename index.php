c214dde689b3d56081b6fd96bd33f2dc903d5ecc
wordpressmonk <kevin@wordpressmonk.com>
2015-09-04 10:59:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
-echo "<h1 align='center' >Testing App Via API</h1>";
+echo "<h1 align='center' >Please Wait Loading...</h1>";
 echo "<br/>";
-echo  "<p align='center' style='color:green'>".$_SERVER['SERVER_NAME']."</p>";
+echo  "<p align='center' style='color:green;display:none;'>".$_SERVER['SERVER_NAME']."</p>";
 $s = $_REQUEST['s'];
 $u = $_REQUEST['u'];
 if($s!='' && $u!=''){
@@ -12,6 +12,4 @@ $url = "http://retargetbomb.herokuapp.com/link/";
 //file_get_content($url);
 header("location:$url");
 exit;
-
-
 ?>
