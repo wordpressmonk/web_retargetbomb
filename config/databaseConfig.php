321fefc40dd43dc2b12fe0c256464d547017523a
wordpressmonk <kevin@wordpressmonk.com>
2015-09-04 17:48:21 +0530

    Update databaseConfig.php

--- config/databaseConfig.php
+++ config/databaseConfig.php
@@ -2,5 +2,10 @@
 error_reporting(E_ALL & ~E_NOTICE);
 session_start();
 //create a db  connection  via pdo 
-
+define("servername", getenv("servername"));
+define("database", getenv("database"));
+define("username", getenv("username"));
+define("password", getenv("password"));
+define("developer", true);
 ?>
+
